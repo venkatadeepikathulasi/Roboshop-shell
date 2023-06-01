b311ab4e3dae44812b6fb43685e201171d8c4df3
Deepika <venkatadeepikathulasi@gmail.com>
2023-06-01 12:08:48 +0530

    adding changes in redis

--- redis.sh
+++ redis.sh
@@ -4,7 +4,7 @@ echo -e "\e[32m >>> install redis >>>>\e[0m"
 dnf module enable redis:remi-6.2 -y
 yum install redis -y
 echo -e "\e[32m >>>update redis >>>>\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf/etc//redis/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf/etc/redis/redis.conf
 echo -e "\e[32m >>> start redis >>>>\e[0m"
 systemctl enable redis
 systemctl start redis
