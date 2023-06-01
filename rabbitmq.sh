a4b004ca5a231fb369930bb4fb1ede3406943532
Deepika <venkatadeepikathulasi@gmail.com>
2023-06-01 15:13:22 +0530

    adding changes in rabbit mq

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,7 +1,17 @@
+echo -e "\e[32m >>>>>>>>setup erlang >>>>>>>\e[0m"
+
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+echo -e "\e[32m >>>>>>>>setup rabbitmq>>>>>>>\e[0m"
+
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
-yum install rabbitmq-server -y
+echo -e "\e[32m >>>>>>>>install rabbitmq>>>>>>>\e[0m"
+
+yum install erlang rabbitmq-server -y
+echo -e "\e[32m >>>>>>>>start service>>>>>>>\e[0m"
+
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
+echo -e "\e[32m >>>>>>>>add application user>>>>>>>\e[0m"
+
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
