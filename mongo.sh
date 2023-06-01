bec31c99fd7e1bf0b1dfbf9f21a056916a41cb88
Deepika <venkatadeepikathulasi@gmail.com>
2023-06-01 09:39:58 +0530

    updated mongod config file

--- mongo.sh
+++ mongo.sh
@@ -3,4 +3,5 @@ yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+sed -i -e 's|127.0.0.1 | 0.0.0.0' /etc/mongod.conf
 systemctl restart mongod
