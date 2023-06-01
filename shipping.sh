977537f80c2b2cd240fe7d33c1ad036add029f26
Deepika <venkatadeepikathulasi@gmail.com>
2023-06-01 14:56:15 +0530

    adding changes in shipping

--- shipping.sh
+++ shipping.sh
@@ -1,15 +1,23 @@
+echo -e "\e[32m >>>>>>>>install maven>>>>>>>\e[0m"
 yum install maven -y
+echo -e "\e[32m >>>>>>>>adding roboshop>>>>>>>\e[0m"
 useradd roboshop
+echo -e "\e[32m >>>>>>>>making directory>>>>>>>\e[0m"
+rm -rf /app
 mkdir /app
+echo -e "\e[32m >>>>>>>>Download app content>>>>>>>\e[0m"
 curl -L -o /tmpshi/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+echo -e "\e[32m >>>>>>>>unzip the file>>>>>>>\e[0m"
 cd /app
 unzip /tmp/shipping.zip
-cd /app
+
+echo -e "\e[32m >>>>>>>>download maven dependcies>>>>>>>\e[0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
+echo -e "\e[32m >>>>>>>>install my sql>>>>>>>\e[0m"
+yum install mysql -y
+mysql -h mysql.devops1008.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+echo -e "\e[32m >>>>>>>>start the service>>>>>>>\e[0m"
 systemctl daemon-reload
 systemctl enable shipping
 systemctl start shipping
-yum install mysql -y
-mysql -h mysql.devops1008.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
-systemctl restart shipping
