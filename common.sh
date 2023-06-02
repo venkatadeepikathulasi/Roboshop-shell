fd823fc99c16b9bb475b1c49102f7b876678ec0a
Deepika <venkatadeepikathulasi@gmail.com>
2023-06-02 18:44:10 +0530

    adding changes in payment

--- common.sh
+++ common.sh
@@ -1,27 +1,30 @@
 app_user = roboshop
-
+print_head()
+{
+  echo -e\e[32m>>>>>>>$*<<<<<<<<<<<\e[0m
+}
 func_nodejs()
 {
-  echo -e "\e[31m >>>>>>>>configuration repos>>>>>>>\e[0m"
+ print_head "configuration repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-  echo -e "\e[32m>>>>>>>> install nodejs>>>>>>>>>\e[0m"
+  print_head "install nodejs"
   yum install nodejs -y
-  echo -e "\e[33m>>>>>>>>add application user >>>>>>>\e[0m"
+  print_head "add application user"
   useradd roboshop
-  echo -e "\e[34m>>>>>> making directory>>>>>>\e[0m"
+  print_head "making directory"
   # before creating the app directory some od content we have to remove
   rm-rf /app
   mkdir /app
-  echo -e "\e[35m>>>>>> download app content>>>>>>\e[0m"
+  print_head "download app content"
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
   cd /app
-  echo -e "\e[36m>>>>>> unzip the file>>>>>>\e[0m"
+ print_head "unzip the file"
   unzip /tmp/${component}.zip
-  echo -e "\e[33m>>>>>> install nodejs repos >>>>>>\e[0m"
+  print_head "install nodejs repos"
   npm install
-  echo -e "\e[34m>>>>>> copy cart systemd service file>>>>>>\e[0m"
+  print_head "copy cart systemd service file"
   cp $script_path/${component}.service /etc/systemd/system/{component}.service
-  echo -e "\e[35m>>>>>> Start cart service>>>>>>\e[0m"
+  print_head "Start cart service"
   systemctl daemon-reload
   systemctl enable ${component}
   systemctl start ${component}
